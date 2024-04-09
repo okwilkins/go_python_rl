8d30572eb604398324470490a73b6bf31da3cba5
Oliver Wilkins <okwilkins@googlemail.com>
2024-04-09 22:54:44 +0000

    feat: Add PPO model training using stable_baselines3

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from python_xo.environment import (
     NaughtsAndCrossesEnvironment,
+    NaughtsAndCrossesEnvironmentGym,
     Owner,
 )
 from python_xo.agent import MinMaxAgent
 
 # import random
-# from stable_baselines3 import PPO, DQN
+from stable_baselines3 import PPO
+
 # from stable_baselines3.common.evaluation import evaluate_policy
+
 # import argparse
 
 
@@ -35,47 +38,23 @@ def run_simulation() -> None:
 
 
 def main() -> None:
-    for _ in range(1):
-        run_simulation()
+    agent = MinMaxAgent(agent_mark=Owner.NAUGHT)
+    env = NaughtsAndCrossesEnvironment(
+        board=[
+            [Owner.EMPTY, Owner.EMPTY, Owner.EMPTY],
+            [Owner.EMPTY, Owner.EMPTY, Owner.EMPTY],
+            [Owner.EMPTY, Owner.EMPTY, Owner.EMPTY],
+        ],
+        time_step=0,
+        user_mark=Owner.CROSS,
+        agent=agent,
+    )
+    gym_env = NaughtsAndCrossesEnvironmentGym(env)
+    gym_env.reset()
+
+    model = PPO("MlpPolicy", gym_env, verbose=2)
+    model.learn(10_000, progress_bar=True)
 
 
 if __name__ == "__main__":
     main()
-    # env = NaughtsAndCrossesEnvironment(
-    #     board=[
-    #         [Owner.EMPTY, Owner.EMPTY, Owner.EMPTY],
-    #         [Owner.EMPTY, Owner.EMPTY, Owner.EMPTY],
-    #         [Owner.EMPTY, Owner.EMPTY, Owner.EMPTY],
-    #     ],
-    #     agent=ag
-    # )
-    # gym_env = NaughtsAndCrossesEnvironmentGym(env)
-    # model = PPO("MlpPolicy", gym_env, verbose=1)
-
-    # model = PPO.load("ppo_naughts_and_crosses", env=gym_env)
-    # mean_reward, std_reward = evaluate_policy(
-    #     model,
-    #     gym_env,
-    #     n_eval_episodes=1000,
-    #     deterministic=True,
-    # )
-    # print(f'Mean reward: {mean_reward} +/- {std_reward}')
-
-    # obs = gym_env.naughts_and_crosses_environment.observation()
-    # terminated = False
-
-    # while not terminated:
-    #     gym_env.render()
-    #     t = input("Move: ")
-    #     row = int(t) // 3
-    #     col = int(t) % 3
-    #     gym_env.naughts_and_crosses_environment.board[row][col] = Owner.NAUGHT
-
-    #     action, _ = model.predict(obs, deterministic=True)
-    #     obs, reward, terminated, _, _ = gym_env.step(action)
-    #     print(f'Reward: {reward}')
-
-    # gym_env.render()
-
-    # model.learn(total_timesteps=1_000_000)
-    # model.save("ppo_naughts_and_crosses")
