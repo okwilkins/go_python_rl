a47db62e5f255140f47bedf5f6c24138df348225
Oliver Wilkins <okwilkins@googlemail.com>
2024-04-12 21:44:19 +0000

    feat: Add test for MinMaxAgent not game over

--- go_xo/tests/agent_test.go
+++ go_xo/tests/agent_test.go
@@ -32,6 +32,26 @@ func TestMinMaxAgentGameOver(t *testing.T) {
 		}
 	}
 }
+
+func TestMinMaxAgentNotGameOver(t *testing.T) {
+	agent := xo.MinMaxAgent{
+		AgentMark:    xo.Naught,
+		OpponentMark: xo.Cross,
+	}
+
+	game_over_observations := [][9]int{
+		{0, 0, 0, 0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0, 0, 0, 1},
+		{0, 0, 1, 0, 2, 0, 0, 0, 1},
+	}
+
+	for _, observation := range game_over_observations {
+		if agent.GameOver(observation) {
+			t.Errorf("Observation %v was supposed to not be game over!", observation)
+		}
+	}
+}
+
 func TestGetIndexOfEmptyCells(t *testing.T) {
 	observations := [][9]int{
 		{1, 0, 2, 0, 1, 0, 2, 0, 1},
